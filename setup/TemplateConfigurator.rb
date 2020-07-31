c233fd711fd70f7079f921073aa7e796313ff5ca
王士昌 <wwwarehouse@163.com>
2020-07-31 11:38:54 +0800

    TODO:// 1、

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -27,6 +27,45 @@ module Pod
       end
       answer
     end
+    
+    def ask_with_answers(question, possible_answers)
+
+      print "\n#{question}? ["
+
+      print_info = Proc.new {
+
+        possible_answers_string = possible_answers.each_with_index do |answer, i|
+           _answer = (i == 0) ? answer.underlined : answer
+           print " " + _answer
+           print(" /") if i != possible_answers.length-1
+        end
+        print " ]\n"
+      }
+      print_info.call
+
+      answer = ""
+
+      loop do
+        @message_bank.show_prompt
+        answer = gets.downcase.chomp
+
+        answer = "yes" if answer == "y"
+        answer = "no" if answer == "n"
+
+        # default to first answer
+        if answer == ""
+          answer = possible_answers[0].downcase
+          print answer.yellow
+        end
+
+        break if possible_answers.map { |a| a.downcase }.include? answer
+
+        print "\nPossible answers are ["
+        print_info.call
+      end
+
+      answer
+    end
 
     def run
       @message_bank.welcome_message
