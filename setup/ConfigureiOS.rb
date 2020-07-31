ca8e62109efe8ea8557f76ded10dfea2eaec29fd
王士昌 <wwwarehouse@163.com>
2020-07-31 11:25:28 +0800

    TODO:// 1、+2

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -22,6 +22,7 @@ module Pod
       
 
       prefix = nil
+      platform = nil
 
       loop do
           puts '--------- 王大仙最帅 ---------'.green
@@ -34,26 +35,27 @@ module Pod
           break
         end
 #        platform = configurator.ask("What is your class prefix")
-        
+        puts '--- 模板工程正在加载中，请稍后 ---'.green
+        platform = configurator.ask_with_answers("输入想要的平台demo", ["ios", "osx"]).to_sym
         
       end
       
 
-#      Pod::ProjectManipulator.new({
-#        :configurator => @configurator,
-#        :xcodeproj_path => "templates/ios/Example/PROJECT.xcodeproj",
-#        :platform => :ios,
-#        :remove_demo_project => (keep_demo == :no),
-#        :prefix => prefix
-#      }).run
-      
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
-        :xcodeproj_path => "templates/ios/Example-osx/PROJECT.xcodeproj",
-        :platform => :osx,
+        :xcodeproj_path => "templates/ios/Example/PROJECT.xcodeproj",
+        :platform => :platform,
         :remove_demo_project => (keep_demo == :no),
         :prefix => prefix
       }).run
+      
+#      Pod::ProjectManipulator.new({
+#        :configurator => @configurator,
+#        :xcodeproj_path => "templates/ios/Example-osx/PROJECT.xcodeproj",
+#        :platform => :osx,
+#        :remove_demo_project => (keep_demo == :no),
+#        :prefix => prefix
+#      }).run
 
       # There has to be a single file in the Classes dir
       # or a framework won't be created, which is now default
